a0ac5dd7095411a2b84545bbce9421baa3cf5f75
Chris Wheatley <chriswheatley87@gmail.com>
2015-03-14 23:42:54 +0000

    basic angular2 app

--- app/templates/src/_index.js
+++ app/templates/src/_index.js
@@ -1,1 +1,22 @@
 import {Component, Template, bootstrap} from 'angular2/angular2';
+import {bind} from 'angular2/di';
+
+@Component({
+  selector: 'generator-angular2'
+})
+
+@Template({
+  url: 'generator-angular2.html'
+})
+
+class GeneratorAngular2 {
+
+  constructor() {
+    console.log('component mounted');
+  }
+
+}
+
+export function main() {
+  bootstrap(GeneratorAngular2);
+}
